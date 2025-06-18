7459ea1dbb510b2e7b5ed170933c67c1df91751d
cMendoncaaa <cauamendooncaa55@gmail.com>
2025-06-18 00:01:16 -0300

    feat: adiciona serviço de criação de usuários

---
+++ src/model/createUser.ts
@@ -0,0 +1,49 @@
+import { validateCpf } from "./validateData/validadeteCpf.js";
+import { Candidate } from "./class/candidate.js";
+import { validateAge } from "./validateData/validateAge.js";
+import * as DB from "../server/data/connect.js";
+export let createCanditado = (user: {
+  name: string;
+  email: string;
+  confirme_email: string;
+  senha: number;
+  confirme_senha: number;
+  telefone: string;
+  cpf: string;
+  data_nascimento: Date;
+}) => {
+  let newUser = new Candidate(
+    user.name,
+    user.email,
+    user.confirme_email,
+    user.senha,
+    user.confirme_senha,
+    user.telefone,
+    user.cpf,
+    user.data_nascimento
+  );
+
+  if (newUser) {
+    let cpfIsValid: boolean = validateCpf(newUser.cpf);
+    let dateIsValid: boolean = validateAge(newUser.data_nascimento);
+    let emailIsValid: boolean = newUser.email === newUser.confirme_email;
+    let passwordIsValid: boolean = newUser.senha === newUser.confirme_senha;
+
+
+    try {
+      if (!cpfIsValid) throw new Error("CPF inválido");
+      if (!dateIsValid) throw new Error("Data de nascimento inválida");
+      if (!emailIsValid) throw new Error("Emails não coincidem ou inválidos");
+      if (!passwordIsValid)
+        throw new Error("Senhas não coincidem ou inválidas");
+
+      DB.pool.query(
+        `INSERT INTO tb_candidato (name, email, senha, telefone, cpf, data_nascimento) VALUES ($1, $2, $3, $4, $5, $6)`,
+        [newUser.name, newUser.email, newUser.telefone, newUser.cpf, newUser.data_nascimento]
+      )
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+};
