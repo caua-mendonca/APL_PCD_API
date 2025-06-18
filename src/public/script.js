2d634363df5f45a9030cedab1c145012bc2de1bf
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-18 00:19:51 -0300

    chore: removendo pasta public

--- src/public/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggleContrast').addEventListener('click', function() {
-    document.body.classList.toggle('high-contrast');
-});
-
-document.getElementById('submitBtn').addEventListener('click', function() {
-    alert('Formulário enviado com sucesso!');
-});
