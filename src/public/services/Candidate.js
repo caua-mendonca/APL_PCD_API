9185d0f19a3d57f484f9fec9621ba15018827a4b
cMendoncaaa <cauamendooncaa55@gmail.com>
2025-06-18 00:03:17 -0300

    feat: serviço de manipulação de dados de candidatos

---
+++ src/public/services/Candidate.js
@@ -0,0 +1,15 @@
+class Candidate {
+  id;
+  name;
+  address;
+  city;
+  birthday;
+
+  constructor(id, name, address, city, birthday) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.city = city;
+    this.birthday = birthday;
+  }
+}
