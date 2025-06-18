29a650773226d6723e46cadb61d7bbb6b03479ee
cMendoncaaa <cauamendooncaa55@gmail.com>
2025-06-18 00:03:22 -0300

    feat: configura conexão com o banco de dados Oracle

---
+++ src/server/data/connect.ts
@@ -0,0 +1,18 @@
+// server/data/connect.ts (ou .js)
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+pool.on("connect", () => {
+  console.log("✅ Conectado ao PostgreSQL via Pool!");
+});
+
+pool.on("error", (err) => {
+  console.error("❌ Erro inesperado no pool de conexões:", err);
+});
