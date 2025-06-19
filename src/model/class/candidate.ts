14f4826d3d4a33e85d760327aa426a50ab415183
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-19 14:45:47 -0300

    feat(model): define novos atributos de acessibilidade e deficiência na classe Candidate

--- src/model/class/candidate.ts
+++ src/model/class/candidate.ts
@@ -1,29 +1,54 @@
 export class Candidate {
-  public name: string;
-  public email: string;
-  public confirme_email: string;
-  public senha: number;
-  public confirme_senha: number;
-  public telefone: string;
-  public cpf: string;
-  public data_nascimento: Date;
-  constructor(
-    name: string,
-    email: string,
-    confirme_email: string,
-    senha: number,
-    confirme_senha: number,
-    telefone: string,
-    cpf: string,
-    data_nascimento: Date
+      public name: string
+      public email: string
+      public confirme_email: string
+      public senha: string
+      public confirme_senha: string
+      public telefone: string
+      public cpf: string
+      public data_nascimento: Date
+      public def_visual: boolean
+      public def_auditiva: boolean
+      public def_fisica: boolean
+      public def_intelectual: boolean
+      public outra_def: boolean
+      public descricao_def: string
+      public acessibilidade_trab: boolean
+      public descricao_acessibilidade: string
+  constructor(      name: string,
+      email: string,
+      confirme_email: string,
+      senha: string,
+      confirme_senha: string,
+      telefone: string,
+      cpf: string,
+      data_nascimento: Date,
+      def_visual: boolean,
+      def_auditiva: boolean,
+      def_fisica: boolean,
+      def_intelectual: boolean,
+      outra_def: boolean,
+      descricao_def: string,
+      acessibilidade_trab: boolean,
+      descricao_acessibilidade: string
+
   ) {
-    this.name = name;
-    this.email = email;
-    this.confirme_email = confirme_email;
-    this.senha = senha;
-    this.confirme_senha = confirme_senha;
-    this.telefone = telefone;
-    this.cpf = cpf;
-    this.data_nascimento = data_nascimento;
+    this.name = name
+    this.email = email
+    this.confirme_email = confirme_email
+    this.senha = senha
+    this.confirme_senha = confirme_senha    
+    this.telefone = telefone
+    this.cpf = cpf    
+    this.data_nascimento = data_nascimento
+    this.def_visual = def_visual
+    this.def_auditiva = def_auditiva
+    this.def_fisica = def_fisica
+    this.def_intelectual = def_intelectual
+    this.outra_def = outra_def
+    this.descricao_def = descricao_def
+    this.acessibilidade_trab = acessibilidade_trab
+    this.descricao_acessibilidade = descricao_acessibilidade
+
   }
 }
