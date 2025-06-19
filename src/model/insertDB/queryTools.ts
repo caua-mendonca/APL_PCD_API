145f00973ddf2c6642dec6ab6284001d48bbb2ea
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-19 00:47:24 -0300

    feat(db): adicionar query para insert de empresa no banco

--- src/model/insertDB/queryTools.ts
+++ src/model/insertDB/queryTools.ts
@@ -14,3 +14,19 @@ export let insertIntoCandidate = async (user: {
     [user.name, user.email, user.senha , user.telefone, user.cpf, user.data_nascimento]
   );
 };
+
+export let insertIntoContratante = async (user: {
+  nome_fantasia: string;
+  razao_social: string;
+  email: string;
+  confirme_email: string;
+  senha: number;
+  confirme_senha: number;
+  cnpj: string;
+  telefone: string;
+}) => {
+  DB.pool.query(
+    `INSERT INTO tb_empresa (nome_fantasia, razao_social, email, senha, cnpj, telefone) VALUES ($1, $2, $3, $4, $5, $6)`,
+    [user.nome_fantasia, user.razao_social, user.email, user.senha , user.cnpj, user.telefone]
+  );
+};
