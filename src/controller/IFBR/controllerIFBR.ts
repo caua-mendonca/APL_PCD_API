fa08cb6086bdcd5f62172d7dc60b19fe833c6cbf
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-22 21:09:00 -0300

    feat(controller): adiciona controller responsável pelo tratamento do formulário IFBR

---
+++ src/controller/IFBR/controllerIFBR.ts
@@ -0,0 +1,24 @@
+import * as IFBR from "../../model/createIFBR/createIFBR.js";
+
+export let controllerIFBR = async (
+  ID: string,
+  body: [{ id: number; name: string; score: number }]
+): Promise<Boolean> => {
+  try {
+    if (ID === "1") {
+      let ressult = IFBR.createIFBRCompleto(body);
+      if (await ressult === true) {
+        return true;
+      }else{
+        return false
+      }
+    } else if (ID === "2") {
+      return true;
+    } else {
+      throw new Error("ID inválido");
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
