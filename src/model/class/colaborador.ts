de0a35d7ad1b1c988a08460a502ad265df92f87b
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-25 20:26:02 -0300

    feat(model): criar classe Colaborador com propriedades da entidade

---
+++ src/model/class/colaborador.ts
@@ -0,0 +1,14 @@
+export class Colaborador{
+    public name:string
+    public email:string
+    public senha:string
+    public setor:string
+
+    constructor(name:string, email:string,senha:string, setor:string){
+        this.name = name;
+        this.email = email;
+        this.senha = senha
+        this.setor = setor
+    }
+    
+}
