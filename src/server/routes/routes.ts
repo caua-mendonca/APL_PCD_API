ad18ff8b9d82a621ab536e2dd37a513dc6f9bf0f
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-25 20:26:48 -0300

    chore(routes): mapear rotas de colaborador na API

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -1,3 +1,4 @@
 export let createCanditado:string = "/createCanditado";
 export let createContratante:string = "/createContratante";
 export let formIFBR:string = "/formIFBR/:id"
+export let createColaborador:string = "/createColaborador";
