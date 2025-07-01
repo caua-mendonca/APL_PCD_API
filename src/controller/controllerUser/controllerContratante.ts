c26a1d8cd169866ac658ce3cb601f68b555d996d
Diego Melo <diegohenriquemelo14@gmail.com>
2025-06-30 23:51:06 -0300

    refactor(controller): reorganiza controller de contratante conforme padrão MVC

--- src/controller/controllerUser/controllerContratante.ts
+++ src/controller/controllerUser/controllerContratante.ts
@@ -1,7 +1,7 @@
 import { createContratante } from "../../model/createUser/createUser.js";
 
 export let controllerContratante = (user:{
-         nome_fantasia: string, razao_social: string, email: string, confirme_email: string, senha: number, confirme_senha: number, cnpj: string, telefone: string
+         nome_fantasia: string, razao_social: string, email: string, confirme_email: string, senha: string, confirme_senha: string, cnpj: string, telefone: string
 }) => {
 
     let response = createContratante(user);
