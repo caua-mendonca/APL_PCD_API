4be6e62a387511cfadf4baab1c7f21e9ea012a63
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-15 17:03:38 -0300

    feat(controller): implementa método GET para recuperação de dados do contratante

--- src/controller/controllerUser/controllerContratante.ts
+++ src/controller/controllerUser/controllerContratante.ts
@@ -1,4 +1,7 @@
 import { createContratante } from "../../model/createUser/createUser.js";
+import * as DB from "../../repository/insertDB/queryTools.js";
+import { getUser } from "../../model/getUser/getUser.js";
+import { get } from "http";
 
 export let controllerContratante = (user:{
          nome_fantasia: string, razao_social: string, email: string, confirme_email: string, senha: string, confirme_senha: string, cnpj: string, telefone: string
@@ -9,3 +12,15 @@ export let controllerContratante = (user:{
     return response
 };
 
+export let controllerGetContratante = async () => {
+    console.log("Passando ao controllerGetContratante()")
+    let result = await getUser("contratante");
+    // if (result.rows.length > 0) {
+    //     return result;
+    // } else {
+    //     return { message: "Nenhum contratante encontrado." };
+    // }
+
+    console.log("Contratantes encontrados:", result.rows);
+    return result.rows;
+}
