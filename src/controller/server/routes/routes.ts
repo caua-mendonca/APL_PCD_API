f05a20230b0142574639cdaab34171c42cdd0542
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-15 14:02:18 -0300

    chore(routes): registra rota GET para recuperação de usuário por ID

--- src/controller/server/routes/routes.ts
+++ src/controller/server/routes/routes.ts
@@ -1,6 +1,10 @@
 export let createCanditado:string = "/createCanditado";
 export let getCanditado:string = "/getCanditado";
 export let getCanditadoById:string = "/getCanditadoById/:id";
+export let deleteCanditado:string = "/deleteCanditado/:id";
+
 export let createContratante:string = "/createContratante";
+
 export let formIFBR:string = "/formIFBR/:id"
+
 export let createColaborador:string = "/createColaborador";
