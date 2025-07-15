f903de15a9f78499dd6b0472120582322e0ca0c5
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-15 12:26:57 -0300

    feat(routes): adiciona rota GET /candidato/:id para busca individual

--- src/controller/server/routes/routes.ts
+++ src/controller/server/routes/routes.ts
@@ -1,5 +1,6 @@
 export let createCanditado:string = "/createCanditado";
 export let getCanditado:string = "/getCanditado";
+export let getCanditadoById:string = "/getCanditadoById/:id";
 export let createContratante:string = "/createContratante";
 export let formIFBR:string = "/formIFBR/:id"
 export let createColaborador:string = "/createColaborador";
