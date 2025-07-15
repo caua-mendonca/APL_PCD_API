98a07a01b7b135c0dfd0cf6c5f515f0c2357cb35
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-15 07:30:12 -0300

    chore(controller): adiciona logs de acompanhamento de CRUD em controllerContratante

--- src/controller/controllerUser/controllerContratante.ts
+++ src/controller/controllerUser/controllerContratante.ts
@@ -3,7 +3,7 @@ import { createContratante } from "../../model/createUser/createUser.js";
 export let controllerContratante = (user:{
          nome_fantasia: string, razao_social: string, email: string, confirme_email: string, senha: string, confirme_senha: string, cnpj: string, telefone: string
 }) => {
-
+    console.log("Passando ao createContratante()")
     let response = createContratante(user);
 
     return response
