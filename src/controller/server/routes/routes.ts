a83561ea63e3f7f93599d5c01e957aeb86fa1163
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-15 11:56:20 -0300

    feat(routes): adiciona rota GET /candidato para buscar candidatos

--- src/controller/server/routes/routes.ts
+++ src/controller/server/routes/routes.ts
@@ -1,4 +1,5 @@
 export let createCanditado:string = "/createCanditado";
+export let getCanditado:string = "/getCanditado";
 export let createContratante:string = "/createContratante";
 export let formIFBR:string = "/formIFBR/:id"
 export let createColaborador:string = "/createColaborador";
