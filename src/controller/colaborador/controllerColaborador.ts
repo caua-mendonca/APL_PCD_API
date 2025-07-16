458f3be75c82f7ba0e3d034415fb6f2f8402a281
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-16 07:35:35 -0300

    feat(controller): implementa método GET para recuperar colaborador

--- src/controller/colaborador/controllerColaborador.ts
+++ src/controller/colaborador/controllerColaborador.ts
@@ -1,4 +1,6 @@
+import { get } from "http";
 import { createColaborador } from "../../model/colaborador/createColaborador.js";
+import * as getUser from"../../model/getUser/getUser.js";
 
 export let controllerColaborador = async (user: {
   name: string;
@@ -12,4 +14,9 @@ export let controllerColaborador = async (user: {
   return response
 };
 
+export let controllerGetColaborador = async (table: string, id: number) => {
+  console.log("Passando ao getColaborador()")
+  let response = await getUser.getColaborador(table, id);
 
+  return response;
+}
