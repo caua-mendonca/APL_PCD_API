bed1962b546c6868234292a00de1b5890e9167c1
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-18 11:04:40 -0300

    feat(validateId): adiciona middleware para validação de ID da vaga

--- src/model/validateId/validateId.ts
+++ src/model/validateId/validateId.ts
@@ -4,3 +4,8 @@ export let validateId = async (id: string) => {
     let validId = await Db.selectId("tb_candidato", id);
     console.log(validId)
 }
+
+export let validateIdContratante = async (id: string):Promise<boolean> => {
+   let arr: string[] = id.toUpperCase().split("-");
+   return arr[0] == "EMP" || arr[0] == "COLAB" ? true :false;
+}
