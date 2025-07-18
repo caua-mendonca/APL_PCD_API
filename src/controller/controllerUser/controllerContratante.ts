c75db5bf0924c79b8382390c06b0b14fff38a49c
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-18 07:04:53 -0300

    refactor(controller-contratante): ajusta regra de negócio conforme novas diretrizes de domínio

--- src/controller/controllerUser/controllerContratante.ts
+++ src/controller/controllerUser/controllerContratante.ts
@@ -6,7 +6,7 @@ import { updateUser } from "../../model/updateUser/updateUser.js";
 
 
 export let controllerContratante = (user:{
-         nome_fantasia: string, razao_social: string, email: string, confirme_email: string, senha: string, confirme_senha: string, cnpj: string, telefone: string
+         nome_fantasia: string, razao_social: string, email: string, confirme_email: string, senha: string, confirme_senha: string, cnpj: string, telefone: string, acessibilidade:string
 }) => {
     console.log("Passando ao createContratante()")
     let response = createContratante(user);
