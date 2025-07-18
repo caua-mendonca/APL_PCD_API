7a1f61c6e7a5a6194a0cc89ddcdc1c08fa139904
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-18 09:25:54 -0300

    refactor(model): redefine entidade Colaborador com base na nova modelagem relacional

--- src/model/class/colaborador.ts
+++ src/model/class/colaborador.ts
@@ -1,14 +1,21 @@
-export class Colaborador{
-    public name:string
-    public email:string
-    public senha:string
-    public setor:string
+export class Colaborador {
+  public id: string;
+  public name: string;
+  public email: string;
+  public senha: string;
+  public setor: string;
 
-    constructor(name:string, email:string,senha:string, setor:string){
-        this.name = name;
-        this.email = email;
-        this.senha = senha
-        this.setor = setor
-    }
-    
+  constructor(name: string, email: string, senha: string, setor: string) {
+    this.id = "";
+    this.name = name;
+    this.email = email;
+    this.senha = senha;
+    this.setor = setor;
+  }
+
+  public async setId(): Promise<void> {
+    let prefix = "COLAB-";
+    let suffix = Math.floor(Math.random() * 1000000);
+    this.id = prefix + suffix;
+  }
 }
