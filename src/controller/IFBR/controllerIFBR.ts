89cb1fd2b355afffeab79887d90355c940947acb
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-18 08:01:47 -0300

    refactor(controller-ifbr): ajusta controle para refletir nova regra de negócio e normalização da tabela de vínculo

--- src/controller/IFBR/controllerIFBR.ts
+++ src/controller/IFBR/controllerIFBR.ts
@@ -4,24 +4,7 @@ export let controllerIFBR = async (
   ID: string,
   body: [{ id: number; name: string; score: number }]
 ): Promise<any> => {
-  try {
-    if (ID === "1") {
-      console.log("Passando ao createIFBR()");
-      let ressult = IFBR.createIFBRCompleto(body);
-      if ((await ressult) === true) {
-        return true;
-      }
-    } else if (ID === "2") {
-      console.log("Passando ao createIFBR()");
-      let ressult = IFBR.createIFBRReduzido(body);
-      if ((await ressult) === true) {
-        return true;
-      }
-    } else {
-      throw new Error("ID inválido");
-    }
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+  let result = IFBR.createIFBRCompleto(body, ID);
+
+  return result
 };
