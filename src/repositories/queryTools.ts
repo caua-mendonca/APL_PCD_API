adc40dd2ccecf00cb000858a241aa2a91f38ee45
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-25 08:39:37 -0300

    refactor(repositories): padronizado acesso aos dados para suporte às novas validações

--- src/repositories/queryTools.ts
+++ src/repositories/queryTools.ts
@@ -604,9 +604,9 @@ export const insertCandidateVaga = async (
 }
 
 
-export let validateData = async (value: string, data:string): Promise<any> => {
+export let validateData = async (value: string, data:string, table: string): Promise<any> => {
   try{
-    let result = await DB.pool.query(`SELECT ${data} FROM tb_candidato WHERE ${data} = $1`, [value]);
+    let result = await DB.pool.query(`SELECT ${data} FROM ${table} WHERE ${data} = $1`, [value]);
     console.log(result.rows.length)
     return result.rows.length
   } catch (error) {
