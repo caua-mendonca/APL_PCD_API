a02fca857194767e0cdc6a4a2bcbbb82ba24b6a7
Diego Melo <diegohenriquemelo14@gmail.com>
2025-07-28 08:06:07 -0300

    feat(queryTools): adicionada query para exclusão de registros de vagas no banco de dados

--- src/repositories/queryTools.ts
+++ src/repositories/queryTools.ts
@@ -390,7 +390,7 @@ export let deleteFromTable = async (
     console.log(`[deleteFromTable] Atualizando status para false no registro ID ${id} da tabela ${table}`);
 
     let result = await DB.pool.query(
-      `UPDATE ${table} SET status_usuario = $1 WHERE id = $2;`,
+      `UPDATE ${table} SET status = $1 WHERE id = $2;`,
       [false, id]
     );
 
