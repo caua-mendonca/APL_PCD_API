0c49259e4026cfd0ef56023a20a14af76cd89192
Diego Melo <diegohenriquemelo14@gmail.com>
2025-08-04 11:26:08 -0300

    feat(routes): adiciona rota DELETE /evento/:id com tratamento de erros e respostas padronizadas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -34,3 +34,4 @@ export let deleteVaga: string = "/deleteVaga/:id";
 // Rotas para o recurso Evento, com ID como parâmetro
 export let createEvento: string = "/createEvento/:id";
 export let getEvento: string = "/getEvento";
+export let deleteEvento: string = "/deleteEvento/:id";
