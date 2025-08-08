d5aeb3545f30baa8617eff3d4c4b2cf8557d11df
Diego Melo <diegohenriquemelo14@gmail.com>
2025-08-08 19:14:35 -0300

    fix(model): ajustar validação de dados no modelEvent

--- src/model/event/modelEvent.ts
+++ src/model/event/modelEvent.ts
@@ -4,7 +4,7 @@ import { Event } from "../entities/class/event.js";
 
 /**
  * Cria um novo evento e insere no banco de dados.
- * 
+ *
  * @param evento - Objeto contendo os dados do evento a ser criado.
  * @param id_calendario - ID do calendário ao qual o evento será associado.
  * @returns Retorna o resultado da operação de inserção no banco.
@@ -21,48 +21,64 @@ export let createEvento = async (
   },
   id_calendario: string
 ) => {
-  // Instancia um novo objeto Event com os dados fornecidos.
-  let newEvent = new Event(
-    evento.titulo,
-    evento.descricao,
-    evento.data,
-    evento.hora_inicio,
-    evento.hora_fim,
-    evento.id_candidato
-  );
+  try {
+    // Instancia um novo objeto Event com os dados fornecidos.
+    let newEvent = new Event(
+      evento.titulo,
+      evento.descricao,
+      evento.data,
+      evento.hora_inicio,
+      evento.hora_fim,
+      evento.id_candidato
+    );
 
-  // Gera um ID único para o evento.
-  newEvent.setId();
-  console.log("🚀 Evento criado com sucesso");
-  console.log("🚀 Inserindo evento no banco de dados");
+    if (
+      evento.hora_inicio.split(":").length !== 2 ||
+      evento.hora_fim.split(":").length !== 2
+    ) {
+      throw new Error("Formato de hora inválido. Use HH:mm.");
+    } else if (
+      Number(evento.hora_inicio.split(":")[0]) >
+      Number(evento.hora_fim.split(":")[0])
+    ) {
+      throw new Error("A hora de início não pode ser maior que a hora de fim.");
+    }
 
-  // Insere o evento na tabela relacionada, vinculando ao calendário.
-  let result = await DB.insertIntoEventos(
-    {
-      id: newEvent.id,
-      titulo: evento.titulo,
-      descricao: evento.descricao,
-      data: evento.data,
-      hora_inicio: evento.hora_inicio,
-      hora_fim: evento.hora_fim,
-      id_candidato: evento.id_candidato,
-    },
-    id_calendario
-  );
+    let idCandidateIsValis = await Validation.validateId(
+      evento.id_candidato,
+      "tb_candidato"
+    );
+    console.log(`ID do candidato válido: ${idCandidateIsValis}`);
+    if (idCandidateIsValis === false || idCandidateIsValis === null) {
+      console.log("❌ ID do candidato inválido.");
+      throw new Error("ID do candidato inválido.");
+    }
 
-  // Valida se a inserção foi bem-sucedida.
-  if (result) {
-    console.log("✔️ Evento inserido com sucesso");
-    return result;
-  } else {
-    console.log("❌ Falha ao inserir evento");
-    throw new Error("Falha ao inserir evento");
+    // Gera um ID único para o evento.
+    newEvent.setId();
+    console.log("🚀 Evento criado com sucesso");
+    console.log("🚀 Inserindo evento no banco de dados");
+
+    // Insere o evento na tabela relacionada, vinculando ao calendário.
+    let result = await DB.insertIntoEventos(newEvent, id_calendario);
+
+    // Valida se a inserção foi bem-sucedida.
+    if (result) {
+      console.log("✔️ Evento inserido com sucesso");
+      return result;
+    } else {
+      console.log("❌ Falha ao inserir evento");
+      throw new Error("Falha ao inserir evento");
+    }
+  } catch (error) {
+    console.error("❌ Erro ao criar evento:", error);
+    throw new Error(`Erro ao criar evento: ${error}`);
   }
 };
 
 /**
  * Recupera todos os eventos da tabela tb_evento.
- * 
+ *
  * @returns Retorna a lista de eventos encontrados no banco.
  */
 export let getEvento = async (id: string) => {
@@ -75,7 +91,7 @@ export let getEvento = async (id: string) => {
 
 /**
  * Exclui (inativa) um evento da tabela tb_evento.
- * 
+ *
  * @param id - ID do evento a ser deletado.
  * @returns Retorna o resultado da operação de exclusão.
  * @throws Lança um erro caso o ID seja inválido ou a exclusão falhe.
