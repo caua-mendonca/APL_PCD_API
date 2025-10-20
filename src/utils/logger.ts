b3cc6fb0ad0f1f3e4ccea7b55bd5f2cb5efc2d1f
Diego Melo <diegohenriquemelo14@gmail.com>
2025-10-19 21:02:50 -0300

    chore: adicionando variedade de logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,21 @@
 import { createLogger, format, transports } from 'winston';
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const logger = createLogger({
-  level: 'info',
+  level: 'silly',
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.errors({ stack: true }),
     format.json()
   ),
-  transports: [
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'info.log', level: 'info' }),
-  ],
+transports: [
+  new transports.File({ filename: 'error.log', level: 'error' }),
+  new transports.File({ filename: 'warn.log', level: 'warn' }),
+  new transports.File({ filename: 'info.log', level: 'info' }),
+  new transports.File({ filename: 'debug.log', level: 'debug' }),
+  new transports.File({ filename: 'combined.log' })
+]
 });
 
 if (process.env.NODE_ENV !== 'production') {
