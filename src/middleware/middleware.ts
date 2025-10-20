71d48bc666479a39b6bd66faf272d076e9d0b97b
Diego Melo <diegohenriquemelo14@gmail.com>
2025-10-19 21:02:10 -0300

    fix: correção no JWT

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,66 +1,73 @@
 import JWT from "jsonwebtoken";
 import dotenv from "dotenv";
+import { logger } from "../utils/logger.js";
 dotenv.config();
 
-/**
- * Middleware para autenticação de Candidatos
- */
 export let authenticateTokenCand = (req: any, res: any, next: any) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401).json({ msg: "Não autorizado!" });
-
-  const token = authHeader.split(" ")[1]; // Bearer <token>
+  console.log("Auth Header:", authHeader);
   const secretCand = process.env.SECRET_CAND as string;
-
+  const token = JWT.sign({ id: 321, role: "candidato" }, secretCand, {
+    expiresIn: "1h",
+  });
+  console.log("Secret EMP:", secretCand);
+  if (!token) {
+    return res.status(401).json({ msg: "Não autorizado!" });
+  }
   try {
     const payload = JWT.verify(token, secretCand);
-    req.user = payload; // Adiciona informações do usuário ao request
-    console.log("Payload decodificado:");
+    console.log("Payload decodificado:", payload);
+    logger.info("Token válido");
     next();
   } catch (error) {
     console.error("Erro JWT:", error);
+    logger.error("Token inválido", { error: error });
     res.status(403).json({ msg: "Token inválido!" });
   }
 };
-
-/**
- * Middleware para autenticação de Empresas
- */
 export let authenticateTokenEmp = (req: any, res: any, next: any) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401).json({ msg: "Não autorizado!" });
-
-  const token = authHeader.split(" ")[1];
+  console.log("Auth Header:", authHeader);
   const secretEmp = process.env.SECRET_EMP as string;
-
+  const token = JWT.sign({ id: 123, role: "empresa" }, secretEmp, {
+    expiresIn: "1h",
+  });
+  console.log("Secret EMP:", secretEmp);
+  if (!token) {
+    return res.status(401).json({ msg: "Não autorizado!" });
+  }
   try {
     const payload = JWT.verify(token, secretEmp);
-    req.user = payload;
-    console.log("Payload decodificado:");
+    console.log("Payload decodificado:", payload);
+    logger.info("Token válido");
     next();
   } catch (error) {
     console.error("Erro JWT:", error);
+    logger.error("Token inválido", { error: error });
     res.status(403).json({ msg: "Token inválido!" });
   }
 };
 
-/**
- * Middleware para autenticação de Administradores
- */
 export let authenticateTokenADM = (req: any, res: any, next: any) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401).json({ msg: "Não autorizado!" });
-
-  const token = authHeader.split(" ")[1];
+  console.log("Auth Header:", authHeader);
   const secretADM = process.env.SECRET_ADM as string;
-
+  const token = JWT.sign({ id: 321, role: "administrador" }, secretADM, {
+    expiresIn: "1h",
+  });
+  console.log("Secret ADM:", secretADM);
+  if (!token) {
+    return res.status(401).json({ msg: "Não autorizado!" });
+  }
   try {
     const payload = JWT.verify(token, secretADM);
-    req.user = payload;
-    console.log("Payload decodificado:");
+    console.log("Payload decodificado:", payload);
+    logger.info("Token válido");
+
     next();
   } catch (error) {
     console.error("Erro JWT:", error);
+    logger.error("Token inválido", { error: error });
     res.status(403).json({ msg: "Token inválido!" });
   }
 };
