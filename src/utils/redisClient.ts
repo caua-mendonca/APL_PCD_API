0368b7cb22249e1abf7c935b2d2a958449a876f6
Diego Melo <diegohenriquemelo14@gmail.com>
2025-10-31 10:12:24 -0300

    feat: implementando Redis

---
+++ src/utils/redisClient.ts
@@ -0,0 +1,36 @@
+import { createClient } from "redis";
+import { logger } from "./logger.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const redisClient = createClient({
+  username: "default",
+  password: String(process.env.REDIS_PASS),
+  socket: {
+    host: String(process.env.REDIS_HOST),
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+redisClient.on("connect", () => {
+  logger.info("Redis client connected");
+});
+
+redisClient.on("error", (err) => {
+  logger.error("Redis client error", err);
+});
+
+(async () => {
+  try {
+    await redisClient.connect();
+
+    await redisClient.set("foo", "bar");
+    const result = await redisClient.get("foo");
+    logger.info(result);
+  } catch (error) {
+    logger.error("Error connecting or using Redis", error);
+  }
+})();
+
+export default redisClient;
